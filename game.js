8069cadf8a275abafecda01843100cc04c69f2ff
Micho-PC <michelhdd27@gmail.com>
2022-05-22 01:36:14 +0300

    Added case for cheating

--- game.js
+++ game.js
@@ -8,16 +8,20 @@ var points = 0;
 
 function load(){
     const startBtn = document.getElementById('start');
-    const wall = document.querySelectorAll('.boundary');
+    const wall = document.querySelectorAll('.boundary:not(.example)');
     const endBtn =  document.getElementById('end');
 
     startBtn.addEventListener('click',start);
 
+    document.getElementById('status').addEventListener('mouseover',cheating);
+    document.getElementsByTagName('p')[0].addEventListener('mouseover',cheating);
+    
     wall.forEach(item => {
         item.addEventListener('mouseover',youLost)
     })
 
     endBtn.addEventListener('mouseover',youWin)
+
 }
 
 //function when the user clicks on S
@@ -25,19 +29,20 @@ function start(){
     if (!game){
         game = true;
         console.log(game);
-        document.querySelectorAll('.boundary').forEach(item => {
+        document.querySelectorAll('.boundary:not(.example)').forEach(item => {
             item.classList.remove('youlose');
         });
         
         document.getElementById("status").innerHTML = 'Begin by moving your mouse over the "S".';
     }
 
+
 }
 
 //function when the user loses the game
 function youLost(){
     if (game){
-        document.querySelectorAll('.boundary').forEach(item => {
+        document.querySelectorAll('.boundary:not(.example)').forEach(item => {
             item.classList.add('youlose');
         });
 
@@ -47,10 +52,21 @@ function youLost(){
     }
 }
 
+//function when the user wins the game
 function youWin(){
     if (game){
         points += 5;
         document.getElementById('status').innerText = `You Win! +5pts Total: ${points}pts`;
         game = false;
     }
+}
+
+//function used in case of cheating
+function cheating(){
+    if (game){
+        alert("Come on, man! Don't cheat. Your points have been reset");
+        points = 0;
+        game = false;
+    }
+
 }
